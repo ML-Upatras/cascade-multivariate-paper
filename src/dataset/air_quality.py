e0899e6f2ce74e76b845243f6b77a7f88f7b7dbb
pantlinardatos <pant.linardatos@gmail.com>
2023-01-08 20:05:22 +0200

    Add updated air_quality

---
+++ src/dataset/air_quality.py
@@ -0,0 +1,41 @@
+import logging
+
+import numpy as np
+import pandas as pd
+
+
+def load_air_quality(csv_path):
+    df = pd.read_csv(csv_path, sep=";")
+    logging.info(f"Shape of dataset: {df.shape}")
+
+    # make all columns lowercase
+    df.columns = [col.lower() for col in df.columns]
+
+    # drop unnamed columns
+    df = df.drop(columns=[col for col in df.columns if "unnamed" in col])
+
+    # convert date to datetime
+    df["time"] = pd.to_datetime(df["time"], format="%H.%M.%S").dt.strftime("%H:%M:%S")
+    df["time"] = df["date"] + " " + df["time"]
+    df["time"] = pd.to_datetime(df["time"])
+    df.drop(["date"], axis=1, inplace=True)
+
+    # replace , with . and convert to float on all columns except time
+    for col in df.columns:
+        if col != "time":
+            df[col] = df[col].replace(",", ".", regex=True).astype(float)
+
+    # replacing null data from -200 to NaN for posterior treatment
+    df = df.replace(-200, np.nan)
+
+    # drop columns with a lot of null values
+    to_drop = ["nox(gt)", "no2(gt)", "nmhc(gt)"]
+    df = df.drop(columns=to_drop)
+
+    # fill with the previous value
+    df = df.fillna(method="ffill")
+
+    # rename vehicle to target
+    df = df.rename(columns={"co(gt)": "ts"})
+
+    return df
