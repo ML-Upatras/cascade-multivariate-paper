29e4dff25c09858d7f7bc9338c8095efc032205f
pantlinardatos <pant.linardatos@gmail.com>
2023-01-14 14:05:20 +0200

    Add comparison tables

--- compare_results.py
+++ compare_results.py
@@ -1,9 +1,13 @@
+from collections import Counter
 from pathlib import Path
 
 import pandas as pd
 
 RESULTS_BASE_DIR = Path("results")
 FRIEDMAN_BASE_DIR = Path("friedman")
+FRIEDMAN_TABLES = FRIEDMAN_BASE_DIR / "tables"
+COMPARISON_TABLES = FRIEDMAN_BASE_DIR / "comparison_tables"
+COMPARISON_TABLES.mkdir(parents=True, exist_ok=True)
 
 datasets = [
     "air_quality",
@@ -18,7 +22,7 @@ datasets = [
     "joho",
     "electricity",
     "iot",
-    "home",
+    # "home",
 ]
 
 if __name__ == "__main__":
@@ -51,3 +55,42 @@ if __name__ == "__main__":
 
     # print percentages of each best model type
     print(best_models["type"].value_counts(normalize=True))
+
+    # load the friedman tables
+    counter = Counter()
+    for table in FRIEDMAN_TABLES.glob("*.csv"):
+        ft = pd.read_csv(table)
+        ft = ft.reset_index(drop=True)
+        ft = ft.drop(columns=["Unnamed: 0"])
+
+        # create a column that indicates if the cascade is better than voting
+        ft["cascade_better"] = ft["cascade"] < ft["voting"]
+
+        # create a column to indicate if the 2nd_model is better than the 1st
+        ft["2nd_better"] = ft["2nd_model"] < ft["model"]
+
+        # create a column to indicate if the 2nd and the cascade are better simultaneously
+        ft["2nd_cascade_better"] = ft["2nd_better"] & ft["cascade_better"]
+
+        # create a row that has the sum of the cascade_better column
+        ft.loc["sum"] = ft.sum()
+
+        # count a how many times the cascade is better than voting
+        counter["cascade_better"] = (
+            counter["cascade_better"] + ft["cascade_better"].loc["sum"]
+        )
+        counter["2nd_better"] = counter["2nd_better"] + ft["2nd_better"].loc["sum"]
+        counter["2nd_cascade_better"] = (
+            counter["2nd_cascade_better"] + ft["2nd_cascade_better"].loc["sum"]
+        )
+        counter["total"] = counter["total"] + len(ft) - 1
+
+        # reset index and replace each value except the last one with the dataset names
+        ft = ft.reset_index()
+        for i, dataset in enumerate(datasets):
+            ft.loc[i, "index"] = dataset
+
+        # save the comparison table
+        ft.to_csv(COMPARISON_TABLES / table.name)
+
+    print(f"Counter: {counter}")
